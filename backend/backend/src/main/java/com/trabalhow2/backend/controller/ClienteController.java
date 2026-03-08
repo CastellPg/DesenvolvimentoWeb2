498c14359bbfe545cb09aa798a2c50ef13c16599
Murilo da Silva Santos <murilo.santos1209@gmail.com>
2026-03-08 20:00:00 -0300

    Começando CRUD de cadastro

---
+++ backend/backend/src/main/java/com/trabalhow2/backend/controller/ClienteController.java
@@ -0,0 +1,29 @@
+package com.trabalhow2.backend.controller;
+
+import com.trabalhow2.backend.controller.request.CadastroClienteRequest;
+import com.trabalhow2.backend.service.ClienteService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cliente")
+public class ClienteController {
+
+    private final ClienteService clienteService;
+
+    @Autowired
+    public ClienteController(ClienteService clienteService) {
+        this.clienteService = clienteService;
+    }
+
+    @PostMapping("/cadastro")
+    public void cadastroCliente(@RequestBody @Valid CadastroClienteRequest request) {
+       clienteService.cadastroCliente(request);
+    }
+
+
+}
---
+++ backend/backend/src/main/java/com/trabalhow2/backend/model/Usuario.java
@@ -0,0 +1,20 @@
+package com.trabalhow2.backend.model;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "usuarios")
+@Getter
+@Setter
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String senha;
+    private String salt;
+    private boolean ativo = true;
+}
