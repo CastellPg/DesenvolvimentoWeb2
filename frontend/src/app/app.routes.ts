5b3c69fbc4454c298f20775f3353485a4724ff9e
Gabriel Silva Costa <gab75698@gmail.com>
2026-03-09 20:25:28 -0300

    adicionado
    add redirecionamento no btn login para page login

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -8,5 +8,5 @@ export const routes: Routes = [
     {path: 'login', component: LoginComponent},
     {path: 'registro', component: RegistroComponent},
     
-    {path: '', redirectTo: 'login', pathMatch: 'full'}
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
 ];
