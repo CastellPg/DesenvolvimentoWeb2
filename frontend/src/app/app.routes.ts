c8f25119cf1c2742117dbc195cfac326095e4251
D0ROX <dannielza16@gmail.com>
2026-03-11 22:40:40 -0300

    Registro + Rota padrão

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
-import {HomeComponent} from './pages/home/home';
-import{LoginComponent} from './pages/login/login';
-import{RegistroComponent} from './pages/registro/registro';
+import { HomeComponent } from './pages/home/home';
+import { LoginComponent } from './pages/login/login';
+import { RegistroComponent } from './pages/registro/registro';
 
 export const routes: Routes = [
-    {path: 'home', component: HomeComponent},
-    {path: 'login', component: LoginComponent},
-    {path: 'registro', component: RegistroComponent},
-    
-    {path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'registro', component: RegistroComponent },
+
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
 ];
