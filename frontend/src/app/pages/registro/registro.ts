a634ab6cf7754961fc9adaadda105e4427ff0a15
D0ROX <dannielza16@gmail.com>
2026-03-15 00:27:37 -0300

    API viaCEP funcionando

--- frontend/src/app/pages/registro/registro.ts
+++ frontend/src/app/pages/registro/registro.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { RouterLink } from '@angular/router';
+
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './registro.html',
   styleUrl: './registro.css',
 })
-export class RegistroComponent {}
+export class RegistroComponent {
+  constructor(private http: HttpClient) {}
+
+  public endereco = {
+    cep: '',
+    logradouro: '',
+    cidade: '',
+    estado: '',
+  };
+
+  buscarCep() {
+    let cepLimpo = this.endereco.cep;
+
+    cepLimpo = cepLimpo.replace(/\D/g, '');
+
+    if (cepLimpo.length === 8) {
+      this.http
+        .get(`https://viacep.com.br/ws/${cepLimpo}/json/`)
+
+        .subscribe((dados: any) => {
+          console.log(dados);
+
+          this.endereco.logradouro = dados.logradouro;
+          this.endereco.cidade = dados.localidade;
+          this.endereco.estado = dados.uf;
+        });
+    } else {
+      console.log('CEP inválido, digite novamente!');
+    }
+  }
+}
