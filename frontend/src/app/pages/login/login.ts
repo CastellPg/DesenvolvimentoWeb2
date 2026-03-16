37f826bfa8c44f872452040a353c962b0b641709
Gabriel Costa <gab75698@gmail.com>
2026-03-16 11:55:58 -0300

    começando a logica de enviar os dados para o back

--- frontend/src/app/pages/login/login.ts
+++ frontend/src/app/pages/login/login.ts
@@ -1,16 +1,35 @@
-import { Component, WritableSignal, Signal, signal } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { Component, WritableSignal, Signal, signal, inject } from '@angular/core';
+import { RouterLink} from "@angular/router";
+import { FormsModule } from "@angular/forms";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
+
 export class LoginComponent {
   hidePassword: WritableSignal<boolean> = signal(true);
 
   togglePassword() {
     this.hidePassword.update(value => !value);
   }
+
+//teste usando ngModele
+  loginUsuario = {
+    email: '',
+    senha: ''
+  };
+
+  private http = inject(HttpClient);
+
+  fazerLogin() {
+    console.log('Pegando os dados do form', this.loginUsuario);
+
+    this.http.post('http://localhost/backend/src/java/', this.loginUsuario)
+  }
+
+
 }
