3f6828d917bfc32cf3a74837ddca3016bf113443
D0ROX <dannielza16@gmail.com>
2026-03-16 11:47:34 -0300

    Mais coisa do regsitro

--- frontend/src/app/pages/registro/registro.ts
+++ frontend/src/app/pages/registro/registro.ts
@@ -1,16 +1,18 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild, ElementRef } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 import { HttpClient } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { NgxMaskDirective } from 'ngx-mask';
 
 @Component({
   selector: 'app-registro',
-  imports: [RouterLink, FormsModule],
+  imports: [RouterLink, FormsModule, NgxMaskDirective],
   templateUrl: './registro.html',
   styleUrl: './registro.css',
 })
 export class RegistroComponent {
+  @ViewChild ('inputNumero') campoNumero!: ElementRef;
   constructor(private http: HttpClient) {}
 
   public endereco = {
@@ -18,9 +20,20 @@ export class RegistroComponent {
     logradouro: '',
     cidade: '',
     estado: '',
+    numero: '',
+    complemento: '',
+  };
+
+  public dados = {
+    cpf: '',
+    nomeCompleto: '',
+    email: '',
+    telefone: ''
   };
 
   buscarCep() {
+    if(!this.endereco.cep) return;
+
     let cepLimpo = this.endereco.cep;
 
     cepLimpo = cepLimpo.replace(/\D/g, '');
@@ -32,12 +45,20 @@ export class RegistroComponent {
         .subscribe((dados: any) => {
           console.log(dados);
 
+          if(dados.erro){
+            console.log('CEP não encontrado!');
+            return;
+          }
+
           this.endereco.logradouro = dados.logradouro;
           this.endereco.cidade = dados.localidade;
           this.endereco.estado = dados.uf;
+
+          setTimeout(() => {
+            this.campoNumero.nativeElement.focus();
+
+          }, 50);
         });
-    } else {
-      console.log('CEP inválido, digite novamente!');
-    }
+    } 
   }
 }
