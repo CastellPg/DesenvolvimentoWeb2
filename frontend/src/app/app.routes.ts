64598396cfd59a482b6466baf644a7bcfcb26689
João Timoteo <joaovictortimoteo518@gmail.com>
2026-03-16 17:29:17 -0300

    Começo tela do cliente

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,12 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home';
 import { LoginComponent } from './pages/login/login';
 import { RegistroComponent } from './pages/registro/registro';
-
+import { Client } from './pages/client/client';
 export const routes: Routes = [
 
     {path: 'home', component: HomeComponent},
     {path: 'login', component: LoginComponent},
     {path: 'registro', component: RegistroComponent},
+    {path: 'client', component: Client},
     
     {path: '', redirectTo: 'login', pathMatch: 'full'},
 ];
