aee6e5f7c8fa52f8748519e8f0d890bb67c460ab
D0ROX <dannielza16@gmail.com>
2026-03-23 11:55:55 -0300

    staff dashboard

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,12 +3,16 @@ import { HomeComponent } from './pages/home/home';
 import { LoginComponent } from './pages/login/login';
 import { RegistroComponent } from './pages/registro/registro';
 import { Client } from './pages/client/client';
+import { StaffDashboardComponent } from './pages/staff-dashboard/staff-dashboard';
+import { OrderListComponent } from './pages/order-list-component/order-list-component'
 export const routes: Routes = [
 
     {path: 'home', component: HomeComponent},
     {path: 'login', component: LoginComponent},
     {path: 'registro', component: RegistroComponent},
     {path: 'client', component: Client},
+    {path: 'staff-Dashboard', component: StaffDashboardComponent},
+    {path: 'funcionario/pedidos', component: OrderListComponent },
     
     {path: '', redirectTo: 'login', pathMatch: 'full'},
 ];
---
+++ frontend/src/app/pages/order-list-component/order-list-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'order-list-component',
+  imports: [],
+  templateUrl: './order-list-component.html',
+  styleUrl: './order-list-component.css',
+})
+export class OrderListComponent {}
