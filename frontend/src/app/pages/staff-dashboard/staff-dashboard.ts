2230629162e2b1c5b68bd081a6ae59e43abee010
D0ROX <dannielza16@gmail.com>
2026-03-30 11:50:31 -0300

    Tela efetuar-orcamento e ajustes na staff-dashboard

--- frontend/src/app/pages/staff-dashboard/staff-dashboard.ts
+++ frontend/src/app/pages/staff-dashboard/staff-dashboard.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common'
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-staff-dashboard',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './staff-dashboard.html',
   styleUrl: './staff-dashboard.css',
 })
@@ -11,12 +12,8 @@ export class StaffDashboardComponent {
 
  solicitacoes = [
   { id: 1, status: 'ABERTA', data: '23/03/2026, 09:30', cliente: 'João Silva', produto: 'Dell Inspiron 15 3000' },
-  { id: 2, status: 'EM ANÁLISE', data: '23/03/2026, 10:45', cliente: 'Carlos Alberto', produto: 'PlayStation 5 - Barulho no Cooler' },
-  { id: 3, status: 'AGUARDANDO PEÇA', data: '22/03/2026, 11:00', cliente: 'Lucas Lima', produto: 'Placa Mãe Asus B450' },
-  { id: 4, status: 'APROVADA', data: '22/03/2026, 14:00', cliente: 'Beatriz Costa', produto: 'MacBook Air - Troca de Bateria' },
-  { id: 5, status: 'PRONTO PARA RETIRADA', data: '21/03/2026, 16:00', cliente: 'Ricardo Gomes', produto: 'Monitor LG 29" - Tela piscando' },
-  { id: 6, status: 'FINALIZADA', data: '20/03/2026, 16:45', cliente: 'Ana Paula', produto: 'iPhone 13 - Troca de Tela' },
-  { id: 7, status: 'REPROVADA', data: '19/03/2026, 09:00', cliente: 'Marcos Souza', produto: 'Notebook HP - Derramou café' }
+  { id: 2, status: 'ABERTA', data: '23/03/2026, 10:45', cliente: 'Carlos Alberto', produto: 'PlayStation 5 - Barulho no Cooler' },
+  { id: 3, status: 'ABERTA', data: '22/03/2026, 11:00', cliente: 'Lucas Lima', produto: 'Placa Mãe Asus B450' },
 ];
 
 efetuarOrcamento(solicitacao: any) {
