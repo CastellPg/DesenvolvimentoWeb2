1606ff1de70b2f50dcc3bd0d4897ef11aae0d82e
D0ROX <dannielza16@gmail.com>
2026-04-13 19:53:48 -0300

    Adcionando interfaces em cagegotia

--- frontend/src/app/services/solicitacao.service.ts
+++ frontend/src/app/services/solicitacao.service.ts
@@ -2,13 +2,19 @@ import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+export interface CategoriaResponse {
+  id: number;
+  nome: string;
+  ativa: boolean;
+}
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SolicitacaoService {
-  
   private http = inject(HttpClient);
   private apiUrl = 'http://localhost:8080/api/solicitacoes';
+  private categoriasUrl = 'http://localhost:8080/api/categorias';
 
   //Busca a lista do cliente
   listarPorCliente(clienteId: number): Observable<any[]> {
@@ -18,5 +24,10 @@ export class SolicitacaoService {
   //Busca os detalhes de apenas uma solicitação
   buscarPorId(id: number): Observable<any> {
     return this.http.get<any>(`${this.apiUrl}/${id}`);
+  }
+
+  //Busca categorias ativas
+  getCategorias(): Observable<CategoriaResponse[]> {
+    return this.http.get<CategoriaResponse[]>(`${this.categoriasUrl}`);
   }
 }
